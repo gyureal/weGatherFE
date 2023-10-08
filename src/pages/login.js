3d9d7c7824342d32a1d8fba5a42bf0d69386a133
gyureal <yonggyu.jeong10@gmail.com>
2023-10-08 14:16:01 +0900

    feat: 회원가입 페이지 추가

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,95 @@
 import React from "react";
+import PageTemplate from "../components/common/Template/pageTemplate/pageTemplate";
+import { Avatar } from "@mui/material";
+import { Typography } from "@mui/material";
+import { TextField } from "@mui/material";
+import { Button } from "@mui/material";
+import { Grid } from "@mui/material";
+import { Link } from "@mui/material";
+import { Box } from "@mui/material";
+import { Checkbox } from "@mui/material";
+import FormControlLabel from '@mui/material/FormControlLabel';
+//icon
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
 
 export default function Login() {
-    return <div>login</div>
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    return (
+        <PageTemplate>
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography variant="subtitle1">
+                    WeGater
+                </Typography>
+
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    로그인
+                </Typography>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address / Nickname"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        로그인
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                비밀번호 찾기
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"회원가입"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </PageTemplate>
+    );
 }
