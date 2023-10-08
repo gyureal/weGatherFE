e5afc8aca9c26e6ab0b3f073a93cbb88bab3cc54
gyureal <yonggyu.jeong10@gmail.com>
2023-10-08 12:52:02 +0900

    refactor: 내비게이션 바 디렉토리 이동

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/navbar/navbar';
+import Navbar from '../components/common/Template/navbar/navbar.js';
 
 export default function Index() {
     return (
