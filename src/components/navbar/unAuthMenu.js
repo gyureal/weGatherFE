de21bd2a0c54600823385f315448d8febaa1d91f
gyureal <yonggyu.jeong10@gmail.com>
2023-10-08 12:29:44 +0900

    feat: 버튼 클릭시 회원가입/ 로그인 페이지로 연결

--- src/components/navbar/unAuthMenu.js
+++ src/components/navbar/unAuthMenu.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Grid } from "@mui/material";
 import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 export default function UnAuthMenu() {
     return (
         <div>
             <Grid container>
-                <Button color="inherit">Login</Button>
-                <Button color="inherit">SignIn</Button>
+                <Button color="inherit"
+                    component={Link}
+                    to={'/sign-up'}
+                >Login
+                </Button>
+                <Button color="inherit"
+                    component={Link}
+                    to={'/sign-in'}
+                >SignIn
+                </Button>
             </Grid>
         </div>
     )
