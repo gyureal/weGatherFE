1e2f8bb7e7d15a0ab4c6ffdb0942c0b333362c7a
gyureal <yonggyu.jeong10@gmail.com>
2023-10-09 15:18:59 +0900

    feat: 계정 메일 인증 알림 추가

---
+++ src/components/common/Template/emailCheckAlert.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Alert, Grid } from '@mui/material';
+import { Link as MuiLink } from "@mui/material";
+import { Link, useNavigate } from 'react-router-dom';
+
+const EmailCheckAlert = (props) => {
+    return (
+        <Alert severity="warning">
+            <Grid container>
+                <Grid item>
+                    WeGather 가입을 완료하려면
+                </Grid>
+                <Grid item>
+                    <MuiLink component={Link} to={"/sign-up"} variant="body2">
+                        계정 인증 메일
+                    </MuiLink>
+                </Grid>
+                <Grid item>
+                    을 확인하세요
+                </Grid>
+            </Grid>
+        </Alert>
+    )
+}
+
+export default EmailCheckAlert;
--- src/components/common/Template/pageTemplate/pageTemplate.js
+++ src/components/common/Template/pageTemplate/pageTemplate.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../navbar/navbar";
 import Footer from "../footer/footer";
+import EmailCheckAlert from "../emailCheckAlert";
 
 const PageTemplate = ({ children }) => (
     <div className="page-template">
         <Navbar />
+        <EmailCheckAlert />
         <main>
             {children}
         </main>
