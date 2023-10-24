66bbb21f815e2485c6a9a667dfc91f983ae2b13c
gyureal <yonggyu.jeong10@gmail.com>
2023-10-24 21:50:28 +0900

    feat: 소모임 Info 추가

--- src/components/smallGroups/base/SmallGroupBase.js
+++ src/components/smallGroups/base/SmallGroupBase.js
@@ -3,12 +3,14 @@ import PageTemplate from '../../common/Template/pageTemplate/pageTemplate';
 import SmallGroupMenu from './SmallGroupMenu';
 import Banner from './Banner';
 import SmallGroupInfo from './SmallGroupInfo';
+import { Grid } from '@mui/material';
 
 function SmallGroupBase({ children }) {
 
     return (
         <PageTemplate>
             <Banner />
+
             <SmallGroupInfo />
             <SmallGroupMenu />
             {children}
--- src/components/smallGroups/base/SmallGroupInfo.js
+++ src/components/smallGroups/base/SmallGroupInfo.js
@@ -1,8 +1,38 @@
+import { Box, Button, Grid, Typography } from '@mui/material'
 import React from 'react'
 
 function SmallGroupInfo() {
     return (
-        <div>SmallGroupInfo</div>
+        <Box marginTop={2}>
+            <Grid container justifyContent="center">
+                <Grid item xs={6}>
+                    <Typography variant='h4' fontWeight="fontWeightMedium">
+                        소모임 이름
+                    </Typography>
+                </Grid>
+                <Grid item xs={4} justifyContent="end" alignItems="end">
+                    <Box display="flex" justifyContent="flex-end">
+                        <Button variant="outlined">
+                            {/* 준비중, 모집중, 모집완료, 모집중지, 종료 */}
+                            상태값
+                        </Button>
+                        <Box marginX={1}></Box>
+                        <Button variant="contained">
+                            {/* 소모임 가입 */}
+                            소모임 가입
+                        </Button>
+                    </Box>
+
+                </Grid>
+            </Grid>
+            <Grid container justifyContent="center">
+                <Grid item xs={10}>
+                    <Typography variant='h6' fontWeight="fontWeightLight">
+                        소모임 짧은 소개입니다.
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Box>
     )
 }
 
