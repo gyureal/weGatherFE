c2814aba5538691078f4283a7e07645fcc63779a
gyureal <yonggyu.jeong10@gmail.com>
2023-10-25 23:51:20 +0900

    feat: 소모임 메뉴 추가

--- src/components/smallGroups/base/SmallGroupMenu.js
+++ src/components/smallGroups/base/SmallGroupMenu.js
@@ -1,8 +1,16 @@
+import { Box, Grid } from '@mui/material'
 import React from 'react'
 
 function SmallGroupMenu() {
     return (
-        <div>SmallGroupMenu</div>
+        <Box marginTop={1}>
+            <Grid container justifyContent="center" xs={10}>
+                <div>소개</div>
+                <div>구성원</div>
+                <div>Gather</div>
+                <div>설정</div>
+            </Grid>
+        </Box>
     )
 }
 
