134fd14aa8a5d27e10f2c84c4ee9e6d84cc32ce0
gyureal <yonggyu.jeong10@gmail.com>
2023-11-02 22:23:22 +0900

    feat: 회원 간단 정보 리스트 템플릿 추가

---
+++ src/components/smallGroups/MemberSimpleInfo.js
@@ -0,0 +1,44 @@
+import { Box, Card, CardMedia, Chip, Typography } from '@mui/material'
+import React from 'react'
+
+function MemberSimpleInfo({ name, introduction, image, isAdmin }) {
+    return (
+        <div>
+            <Card sx={{ display: 'flex', boxShadow: 0 }}>
+                <Box marginRight={3}>
+                    <CardMedia
+                        component="img"
+                        sx={{ width: 64, height: 64 }}
+                        image={image ? image : "https://picsum.photos/id/237/200/300"}
+                        alt="Live from space album cover"
+                    />
+                </Box>
+
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                        <Box marginRight={2}>
+                            <Typography variant="h6" fontWeight="bold">
+                                {name}
+                            </Typography>
+                        </Box>
+                        {
+                            isAdmin ?
+                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                    <Chip label="관리자" color='primary' size="small" />
+                                </Box>
+                                : <Box></Box>
+                        }
+
+                    </Box>
+                    <Box marginTop={1}>
+                        <Typography variant="subtitle2" color="text.secondary" component="div">
+                            {introduction}
+                        </Typography>
+                    </Box>
+                </Box>
+            </Card>
+        </div>
+    )
+}
+
+export default MemberSimpleInfo
--- src/pages/smallGroups/SmallGroupMember.js
+++ src/pages/smallGroups/SmallGroupMember.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import SmallGroupBase from '../../components/smallGroups/base/SmallGroupBase'
 import { useParams } from 'react-router-dom';
+import { Box, Grid } from '@mui/material';
+import MemberSimpleInfo from '../../components/smallGroups/MemberSimpleInfo';
 
 function SmallGroupMember() {
     const { path } = useParams();
 
     return (
         <SmallGroupBase path={path}>
+            <Box marginTop={2}>
+                <Grid container justifyContent="center">
+                    <Grid item xs={10}>
+                        <Box marginY={1}>
+                            <MemberSimpleInfo name={"정우진"} introduction={"반갑습니다 저는 관리자에요!"} isAdmin={true} />
+                        </Box>
+                        <Box marginY={1}>
+                            <MemberSimpleInfo name={"정용규"} introduction={"뭐에요 다들! 놀아봅시다!"} isAdmin={false} />
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
         </SmallGroupBase>
     )
 }
