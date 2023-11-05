f97d2e75cfbd631abca6e17a05066ea6ac97b4e6
gyureal <yonggyu.jeong10@gmail.com>
2023-11-05 12:29:58 +0900

    feat: 프로필 이미지 자르기 기능 추가

--- src/components/common/CropImage.js
+++ src/components/common/CropImage.js
@@ -1,4 +1,4 @@
-import { Box, Button, Card, CardContent, CardHeader, Grid, Typography } from '@mui/material'
+import { Avatar, Box, Button, Card, CardContent, CardHeader, Grid, Typography } from '@mui/material'
 
 import ContentCutIcon from '@mui/icons-material/ContentCut';
 import React, { useRef, useState } from 'react'
@@ -7,40 +7,49 @@ import '/node_modules/cropperjs/dist/cropper.css';
 import { Cropper } from 'react-cropper';
 
 const defaultSrc =
-    "https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg";
+    "https://raw.githubusercontent.com/gyureal/storage/main/image/default-avatar.png";
+
+// DB에 저장된 이미지 
+let savedImage = "";
 
 
 function CropImage() {
-    const [image, setImage] = useState(defaultSrc);
-    const [cropData, setCropData] = useState("#");
+    const [image, setImage] = useState("");
+    const [uploadImage, setUploadImage] = useState("");
     const cropperRef = useRef();
 
-    const getCropData = () => {
+    const onCrop = () => {
         if (cropperRef.current && typeof cropperRef.current.cropper !== "undefined") {
-            setCropData(cropperRef.current.cropper.getCroppedCanvas().toDataURL());
+            setImage(cropperRef.current.cropper.getCroppedCanvas().toDataURL());
         }
     };
 
-    const onImageChange = (event) => {
+    // 이미지 업로드 하여, uploadImage에 담아야함
+    const onImageUpload = (event) => {
         console.log("called");
         console.log("event", event.target.files);
-        // const file = event.target.files[0];
-        // if (file) {
-        //     console.log('file', file);
-        //     setImage(file);
-        // }
+        const file = event.target.files[0];
+        console.log('file ', file);
+        if (file) {
+            const imageUrl = URL.createObjectURL(file);
+            console.log(imageUrl);
+            setUploadImage(imageUrl);
+        }
     };
 
+    // 업로드, Cropped 된 사진이 저장되어야함
     const onConfirmClick = () => {
 
     }
 
+    // 업로드 취소 : DB에 저장된 이미지로 되돌리기
     const onCancelClick = () => {
-
+        console.log(savedImage);
+        setImage(savedImage);
     }
 
     return (
-        <Card variant="outlined" sx={{ display: 'flex', flexGrow: 1, borderRadius: 1, border: '1px solid', borderColor: 'grey:500' }}>
+        <Card variant="outlined" sx={{ display: 'flex', flexGrow: 1, borderRadius: 1 }}>
             <Box sx={{
                 display: 'flex', flexGrow: 1, flexDirection: 'column', bgcolor: 'grey.150',
             }}>
@@ -48,32 +57,16 @@ function CropImage() {
                     프로필 이미지
                 </Box>
 
-                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                    <Cropper
-                        ref={cropperRef}
-                        style={{ height: 200, width: "100%" }}
-                        zoomTo={0.5}
-                        initialAspectRatio={1}
-                        preview=".img-preview"
-                        src={image}
-                        viewMode={1}
-                        minCropBoxHeight={10}
-                        minCropBoxWidth={10}
-                        background={false}
-                        responsive={true}
-                        autoCropArea={1}
-                        checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
-                        guides={true}
-                    />
+
+                <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 1 }}>
+                    <Avatar src={image === "" ? defaultSrc : image} sx={{ width: 150, height: 150 }} />
                 </Box>
 
-                <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
-                    <FileUploadButton onImageChange={onImageChange} />
 
-                    <Button component="label" variant="outlined" startIcon={<ContentCutIcon />} sx={{ marginTop: 1 }} color='success'
-                        onClick={getCropData}>
-                        자르기
-                    </Button>
+                <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+                    <FileUploadButton onImageChange={onImageUpload} />
+
+
                     <Button component="label" variant="contained" sx={{ marginTop: 1 }} color='primary'
                         onClick={onConfirmClick}>
                         확인
@@ -82,9 +75,33 @@ function CropImage() {
                         onClick={onCancelClick}>
                         취소
                     </Button>
-                    <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 1 }}>
-                        <img style={{ width: "100%" }} src={cropData} alt="cropped" />
-                    </Box>
+
+                    {
+                        uploadImage != "" ?
+                            <Box sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', marginTop: 1 }}>
+                                <Cropper
+                                    ref={cropperRef}
+                                    style={{ height: 200, width: "100%" }}
+                                    zoomTo={0.5}
+                                    initialAspectRatio={1}
+                                    preview=".img-preview"
+                                    src={uploadImage}
+                                    viewMode={1}
+                                    minCropBoxHeight={10}
+                                    minCropBoxWidth={10}
+                                    background={false}
+                                    responsive={true}
+                                    autoCropArea={1}
+                                    checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+                                    guides={true}
+                                />
+                                <Button component="label" variant="contained" startIcon={<ContentCutIcon />} sx={{ marginTop: 1 }} color='success'
+                                    onClick={onCrop}>
+                                    자르기
+                                </Button>
+                            </Box>
+                            : <div></div>
+                    }
                 </Box>
 
             </Box>
