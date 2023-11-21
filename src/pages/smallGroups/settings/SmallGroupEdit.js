317170152267cc3ba6b77189a8bb9551305994f4
gyureal <yonggyu.jeong10@gmail.com>
2023-11-21 19:31:41 +0900

    feat: 소모임 소개 수정 뷰 추가

--- src/pages/smallGroups/settings/SmallGroupEdit.js
+++ src/pages/smallGroups/settings/SmallGroupEdit.js
@@ -1,13 +1,82 @@
 import React from 'react'
 import SmallGroupSettingBase from './SmallGroupSettingBase'
-import { Box } from '@mui/material'
+import { Box, Button, Container, Typography } from '@mui/material'
+import { FormField } from '../../../components/common/FormField';
+import Editor from '../../../components/common/editor/Editor';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
 
-function SmallGroupEdit() {
+const renderField = (field) => {
+    return (
+        <div>
+            <FormField field={field} />
+        </div>
+    );
+};
+
+const renderEditorField = (field) => {
+    return (
+        <div>
+            <Box sx={{ mt: 3 }}>
+                <Typography variant="body" color="text.secondary">
+                    {field.label}
+                </Typography>
+                <Editor field={field} />
+            </Box>
+        </div>
+    );
+};
+
+const validate = (values) => {
+    const errors = {};
+
+    return errors;
+}
+
+const submitForm = () => {
+
+}
+
+let SmallGroupEdit = (props) => {
+    const { handleSubmit, submitting } = props;
     return (
         <SmallGroupSettingBase currentMenu={'edit'}>
-            <Box>content</Box>
+            <Container maxWidth="md">
+                <Box sx={{ fontSize: 'h4.fontSize', fontWeight: 'regular' }}>
+                    소모임 소개
+                </Box>
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <Field
+                        component={renderField}
+                        name="shortDescription"
+                        label="짧은 소개"
+                        hintText="100자 이내로 소모임을 소개해 주세요."
+                    />
+                    <Field
+                        component={renderEditorField}
+                        name="fullDescription"
+                        label="상세 소개"
+                        hintText="소모임의 목표, 진행방식, 모집 중인 회원에 대한 정보 등을 상세하게 적어주세요."
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        disabled={submitting}
+                        sx={{ mt: 3, mb: 1 }}
+                    >
+                        소모임 만들기
+                    </Button>
+                </Box>
+            </Container>
         </SmallGroupSettingBase>
     )
 }
 
-export default SmallGroupEdit
+const mapStateToProps = () => {
+    return {}        // Redux Form 초기값주기 
+}
+
+SmallGroupEdit = reduxForm({ validate, form: 'SmallGroupEdit' })(SmallGroupEdit);
+SmallGroupEdit = connect(mapStateToProps, null)(SmallGroupEdit);
+export default SmallGroupEdit;
