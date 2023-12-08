9d51efa498bad61430ec69166d420f031471af31
gyureal <yonggyu.jeong10@gmail.com>
2023-12-08 20:21:08 +0900

    feat:`소모임 회원`카드 클릭시 `회원 프로필` 페이지로 redirect

--- src/components/smallGroups/MemberSimpleInfo.js
+++ src/components/smallGroups/MemberSimpleInfo.js
@@ -1,41 +1,50 @@
-import { Box, Card, CardMedia, Chip, Typography } from '@mui/material'
+import { Box, Card, CardActionArea, CardMedia, Chip, Typography } from '@mui/material'
 import React from 'react'
 import { awsPrefix, defaultAvatar } from '../../static/globalVariables'
+import { useNavigate } from 'react-router-dom';
 
-function MemberSimpleInfo({ name, introduction, image, isManager }) {
+const MemberSimpleInfo = ({ name, introduction, image, isManager }) => {
+    const navigate = useNavigate();
+
+    const onCardClick = () => {
+        navigate(`/profile/${name}`);
+    }
+
     return (
         <div>
-            <Card sx={{ display: 'flex', boxShadow: 0 }}>
-                <Box marginRight={3}>
-                    <CardMedia
-                        component="img"
-                        sx={{ width: 64, height: 64 }}
-                        image={image && image != "" ? awsPrefix + image : defaultAvatar}
-                    />
-                </Box>
-
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                        <Box marginRight={2}>
-                            <Typography variant="h6" fontWeight="bold">
-                                {name}
-                            </Typography>
+            <Card sx={{ display: 'flex', boxShadow: 0, mt: 2 }}>
+                <CardActionArea onClick={onCardClick}>
+                    <Box display="flex" flexDirection="row">
+                        <Box marginRight={3}>
+                            <CardMedia
+                                component="img"
+                                sx={{ width: 64, height: 64 }}
+                                image={image && image != "" ? awsPrefix + image : defaultAvatar}
+                            />
                         </Box>
-                        {
-                            isManager ?
-                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                    <Chip label="관리자" color='primary' size="small" />
+                        <Box display="flex" flexDirection="column">
+                            <Box display="flex" flexDirection="row">
+                                <Box marginRight={2}>
+                                    <Typography variant="h6" fontWeight="bold">
+                                        {name}
+                                    </Typography>
                                 </Box>
-                                : <Box></Box>
-                        }
-
+                                {
+                                    isManager ?
+                                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                            <Chip label="관리자" color='primary' size="small" />
+                                        </Box>
+                                        : <Box></Box>
+                                }
+                            </Box>
+                            <Box marginTop={0.7}>
+                                <Typography variant="subtitle2" color="text.secondary" component="div">
+                                    {introduction}
+                                </Typography>
+                            </Box>
+                        </Box>
                     </Box>
-                    <Box marginTop={1}>
-                        <Typography variant="subtitle2" color="text.secondary" component="div">
-                            {introduction}
-                        </Typography>
-                    </Box>
-                </Box>
+                </CardActionArea>
             </Card>
         </div>
     )
