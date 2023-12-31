f8b7c4ae965a37172765ffd0e1305e1df82c06ee
gyureal <yonggyu.jeong10@gmail.com>
2023-12-31 17:38:03 +0900

    feat(profile): 프로필 조회 시, 관심사 표시

--- src/pages/users/Profile.js
+++ src/pages/users/Profile.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { Box, Button, Grid } from '@mui/material';
+import { Box, Button, Grid, Stack } from '@mui/material';
 import EmailIcon from '@mui/icons-material/Email';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import { useDispatch, useSelector } from 'react-redux';
 import { requestProfileByUsername } from '../../slice/memberSlice';
 import UsersBase from './UsersBase';
+import InterestTag from '../../components/common/InterestTag';
 
 const Profile = () => {
     const { username } = useParams();
@@ -55,7 +56,15 @@ const Profile = () => {
             <Box sx={{ fontSize: 'h5.fontSize', fontWeight: 'light' }}>
                 {userProfile.introductionText ?? "한 줄 소개 입력 전 입니다."}
             </Box>
-            <Box sx={{ mt: 8 }}>
+            <Stack sx={{ mt: 2 }} direction="row" spacing={2}>
+                {
+                    userProfile && userProfile.interests ?
+                        userProfile.interests.map((interest, index) => (
+                            <InterestTag interest={interest} key={index} />
+                        )) : <div></div>
+                }
+            </Stack>
+            <Box sx={{ mt: 4 }}>
                 <Grid container>
                     <Grid item xs={1}>
                         <Box sx={{ fontSize: 'h6.fontSize', fontWeight: 'regular' }}>
